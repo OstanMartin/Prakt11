5c1e2b3c4769d8a7c7eef92d8cd8c26dbd990ae4
Алирзаева Зарема <heeyulolisunwon@gmail.com>
2026-01-28 11:23:50 +0300

    h21

---
+++ src/test/java/AddRemoveElementsTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AddRemoveElementsTest {
+    @Test
+    public void AddElementsTest(){
+        WebDriver driver=new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Downloads\\chromedriver-win64\\chromedriver-win64");
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/button")).click();
+        boolean IsDisplay=driver.findElement(By.xpath("//*[@id=\"elements\"]/button")).isDisplayed();
+        Assert.assertTrue(IsDisplay);
+        driver.quit();
+
+
+        //ghgg
+    }
+}
